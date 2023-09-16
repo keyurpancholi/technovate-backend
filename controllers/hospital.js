5e7bbdf6b77ef5bab3933bdf75c2a927ecb9d5e6
Keyur <keyur.pancholi9@gmail.com>
2023-09-16 20:46:15 +0530

    hospital changes made

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -9,47 +9,63 @@ exports.organMatchInitialize = async (req, res, next) => {
   const organ = new OrganDonation({ donorId: id, organQueue: [] });
   try {
     const data = await organ.save();
-    if(!data){
-      const error = new Error("Couldn't initialize organ donation queue")
-      error.statusCode = 400
-      throw error
+    if (!data) {
+      const error = new Error("Couldn't initialize organ donation queue");
+      error.statusCode = 400;
+      throw error;
     }
-    res.status(201).json({data: data})
+    res.status(201).json({ data: data });
   } catch (error) {
-    if(!error.statusCode){
-      error.statusCode = 500
+    if (!error.statusCode) {
+      error.statusCode = 500;
     }
-    next(error)
+    next(error);
   }
 };
 
 exports.createMatch = async (req, res, next) => {
-  const recipient_id = new mongoose.Types.ObjectId(req.body.recipient_id)
-  const donor_id = new mongoose.Types.ObjectId(req.body.donor_id)
-  const organ = req.body.organ
+  const recipient_id = new mongoose.Types.ObjectId(req.body.recipient_id);
+  const donor_id = new mongoose.Types.ObjectId(req.body.donor_id);
+  const organ = req.body.organ;
 
-  const match = new Match({donorId: donor_id, recipientId: recipient_id, organ: organ})
+  const match = new Match({
+    donorId: donor_id,
+    recipientId: recipient_id,
+    organ: organ,
+  });
   try {
-    const data = await match.save()
-    if(!data){
-      const error = new Error("Cant create a match")
-      error.statusCode = 400
-      throw error
+    const data = await match.save();
+    if (!data) {
+      const error = new Error("Cant create a match");
+      error.statusCode = 400;
+      throw error;
     }
-    return res.status(200).json({data: data})
+    return res.status(200).json({ data: data });
   } catch (error) {
-    if(!error.statusCode){
-      error.statusCode = 500
+    if (!error.statusCode) {
+      error.statusCode = 500;
     }
-    next(error)
+    next(error);
   }
-}
+};
 
-exports.getAllMatch = async (req, res, next) => {
-  const matches = await Match.find();
-  const data = await matches.json();
+// exports.getAllMatch = async (req, res, next) => {
+//   const matches = await Match.find();
+//   const data = await matches.json();
 
-  return res.status(200).json({ data: data });
+//   return res.status(200).json({ data: data });
+// };
+
+exports.getAllOrgans = async (req, res, next) => {
+  try {
+    const list = await OrganDonation.find().populate("organQueue");
+    return res.status(200).json({ data: list });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 exports.updateStatus = async (req, res, next) => {
@@ -57,14 +73,21 @@ exports.updateStatus = async (req, res, next) => {
   const id = req.body.id;
 
   try {
-    const resp = await Match.findByIdAndUpdate(
-      { id: id },
-      { $set: { status: status } }
-    );
-    if (!resp) {
-      const error = new Error("Couldnt find a recipient");
-      error.statusCode = 404;
-      throw error;
+    if (status === "Approved") {
+      const resp = await Match.findByIdAndUpdate(
+        { id: id },
+        { $set: { status: status} }
+      );
+      if (!resp) {
+        const error = new Error("Couldnt find a recipient");
+        error.statusCode = 404;
+        throw error;
+      }
+      return res.status(200).json({ data: resp });
+    }
+
+    else{
+
     }
   } catch (error) {
     if (!error.statusCode) {
@@ -102,12 +125,12 @@ exports.login = async (req, res, next) => {
     );
     return res.json(200).json({ token: token });
   } catch (error) {
-    if(!error.statusCode){
-      error.statusCode = 500
+    if (!error.statusCode) {
+      error.statusCode = 500;
     }
-    next(error)
+    next(error);
   }
-}
+};
 
 exports.signup = async (req, res, next) => {
   const { email, password } = req.body;
@@ -116,7 +139,7 @@ exports.signup = async (req, res, next) => {
     const hashedpw = await bcrypt.hash(password, 12);
     const hospital = new Hospital({ email: email, password: hashedpw });
     const resp = await hospital.save();
-    if (!resp){
+    if (!resp) {
       const error = new Error("Failed to create a new account");
       error.statusCode = 400;
       throw err;
@@ -130,4 +153,4 @@ exports.signup = async (req, res, next) => {
     }
     next(error);
   }
-}
+};
--- routes/hospital.js
+++ routes/hospital.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const hospitalControllers = require("../controllers/hospital")
 
-router.get('/matches', hospitalControllers.getAllMatch)
+// router.get('/matches', hospitalControllers.getAllMatch)
 
 router.patch("/match", hospitalControllers.updateStatus)
 
@@ -14,5 +14,7 @@ router.post('/match', hospitalControllers.createMatch)
 router.post("/signup", hospitalControllers.signup)
 
 router.post("/login", hospitalControllers.login)
+
+router.get("/organs", hospitalControllers.getAllOrgans)
 
 module.exports = router
